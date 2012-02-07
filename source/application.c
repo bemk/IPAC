cdef587588dc7a6c145f80244f893c1230d793b2
bemkuivenhoven@gmail.com <bemkuivenhoven@gmail.com@c6d3d5e5-5d5d-ad11-a772-7790f10f8155>
2012-02-07 14:34:53 +0000

    gen: Fixed some comments
    git-svn-id: https://nuts-radio-a3.googlecode.com/svn/branches/bemk@17 c6d3d5e5-5d5d-ad11-a772-7790f10f8155

--- source/application.c
+++ source/application.c
@@ -8,6 +8,12 @@ MUTEX key_lock;
 MUTEX light_lock;
 struct interupt* key_table[12];
 
+/**
+ * \fn light_backlight
+ * \brief A thred used to light the backlight for 10 seconds
+ * \param arg
+ * \brief Pointer to the (unused) arguments
+ */
 THREAD(light_backlight, arg)
 {
         if (NutMutexTrylock(&light_lock) != 0)
@@ -25,15 +31,26 @@ THREAD(light_backlight, arg)
         NutThreadExit();
         for(;;); // Keep compiler from complaining
 }
-
+/**
+ * \fn key_handle
+ * \brief The thread to scan for key events and handle them when occuring
+ * \param arg
+ * \brief Pointer to the (unused) arguments
+ */
 THREAD(key_handle, arg)
 {
         for (;;)
         {
-                if (KbWaitForKeyEvent(1000) == KB_ERROR)
-                        continue;
+//                 if (KbWaitForKeyEvent(1000) == KB_ERROR)
+//                         continue;
 
                 int key_code = KbGetKey();
+                if (key_code != 0)
+                {
+                        btn_pushed();
+                        LcdChar('C');
+                        continue;
+                }
                 NutMutexLock(&key_lock);
                 if (key_table[key_code] != NULL)
                 {
@@ -41,9 +58,16 @@ THREAD(key_handle, arg)
                         key_table[key_code]->handle();
                 }
                 NutMutexUnlock(&key_lock);
+                NutSleep(100);
         }
 }
 
+/**
+ * \fn app_kbd_update_keys
+ * \brief Is used to import a new set of function pointers to the table
+ * \param new_map
+ * \brief The pointer to the new table, gets copied so keep track!
+ */
 int app_kbd_update_keys(struct interupt** new_map)
 {
         NutMutexLock(&key_lock);
@@ -59,6 +83,10 @@ int app_kbd_update_keys(struct interupt** new_map)
         return 0;
 }
 
+/**
+ * \fn app_kbd_start
+ * \brief This starts the thread which handles the keyboard events
+ */
 int app_kbd_start()
 {
         NutMutexInit(&key_lock);
@@ -67,6 +95,10 @@ int app_kbd_start()
         return 0;
 }
 
+/**
+ * \fn btn_pushed
+ * \brief Gets called when the backlight needs to be lit
+ */
 int btn_pushed()
 {
         NutThreadCreate("bkg_light", light_backlight, NULL, 256);
