e22788d99a0241ff68195794c9e6250151a495b8
tybalde@gmail.com <tybalde@gmail.com@c6d3d5e5-5d5d-ad11-a772-7790f10f8155>
2012-02-14 09:34:29 +0000

    mnu: Made a function static (With Bart)
    git-svn-id: https://nuts-radio-a3.googlecode.com/svn/trunk@50 c6d3d5e5-5d5d-ad11-a772-7790f10f8155

--- source/menu.c
+++ source/menu.c
@@ -2,15 +2,11 @@
 #include "menu.h"
 #include <sys/heap.h>
 
+static void tz_menu_init();
+static void klok_menu_init();
+
 char* itoc[24] = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13",
 				  "14","15","16","17","18","19","20","21","22","23"};
-
-void mem_set(void* dst, int c, int size)
-{
-	char* tmp = dst;
-	for (; size > 0; size--, tmp = (void*)((unsigned int)tmp++))
-		tmp = (c & 0xFF);
-}
 
 struct menu *mnu = NULL;
 
@@ -32,7 +28,7 @@ THREAD(mnu_thread, args)
  * \fn std_btn_up
  * \brief Standard move up in the current menu
  */
-void std_btn_up(struct menu* this)
+static void std_btn_up(struct menu* this)
 {
         this->message_id++;
         if(this->message_id >= this->no_messages)
@@ -43,7 +39,7 @@ void std_btn_up(struct menu* this)
  * \fn std_btn_down
  * \brief Standard move down in the current menu
  */
-void std_btn_down(struct menu* this)
+static void std_btn_down(struct menu* this)
 {
         this->message_id--;
         if(this->message_id < 0)
@@ -54,13 +50,13 @@ void std_btn_down(struct menu* this)
  * \fn std_btn_left
  * \brief Standard move to the parent menu
  */
-void std_btn_left(struct menu* this)
+static void std_btn_left(struct menu* this)
 {
 		if (this->parent_ctor != NULL)
 			this->parent_ctor();
 }
 
-void std_btn_right(struct menu* this)
+static void std_btn_right(struct menu* this)
 {
 		switch(mnu->message_id)
 		{
@@ -84,12 +80,8 @@ void std_btn_right(struct menu* this)
 		}
 }
 
-void std_mnu_init()
+static void std_mnu_build()
 {
-	if (mnu == NULL)
-		mnu = malloc(sizeof(*mnu));
-	if (mnu == NULL)
-		return -1;
 	memset(mnu, 0, sizeof(struct menu));
 	mnu->top_line = "Menu";
 	mnu->messages[0] = "Time zones";
@@ -100,28 +92,38 @@ void std_mnu_init()
 	mnu->btn_up = std_btn_up;
 	mnu->btn_down = std_btn_down;
 	mnu->btn_right = std_btn_right;
+}
+
+void std_mnu_init()
+{
+	if (mnu == NULL)
+		mnu = malloc(sizeof(*mnu));
+	if (mnu == NULL)
+		return -1;
+	
+	std_mnu_build();
 	
 	NutThreadCreate("Menu", mnu_thread, NULL, 512);
 }
 
-void tz_menu_init()
+static void tz_menu_init()
 {
 	if (mnu == NULL)
 		return;
 	memset(mnu, 0, sizeof(struct menu));
 	mnu->top_line = "Time zone";
 	mnu->messages[0] = "UTC + %";
-	mnu->parent_ctor = std_mnu_init;
+	mnu->parent_ctor = std_mnu_build;
 	mnu->btn_left = std_btn_left;
 }
 
-void klok_menu_init()
+static void klok_menu_init()
 {
 	if(mnu == NULL)
 		return;
 	memset(mnu, 0, sizeof(struct menu));
 	mnu->top_line = "Klok";
 	mnu->messages[0] = "De tijd";
-	mnu->parent_ctor = std_mnu_init;
+	mnu->parent_ctor = std_mnu_build;
 	mnu->btn_left = std_btn_left;
 }
