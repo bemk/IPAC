481427d261766a3667eca4a629da0f58ae0be08f
remyoman@gmail.com <remyoman@gmail.com@c6d3d5e5-5d5d-ad11-a772-7790f10f8155>
2012-03-27 11:37:29 +0000

    Re-added Store.c after accidently deleting it due to having the wrong file open in Notepad++
    git-svn-id: https://nuts-radio-a3.googlecode.com/svn/trunk@175 c6d3d5e5-5d5d-ad11-a772-7790f10f8155

---
+++ source/Store.c
@@ -0,0 +1,85 @@
+#include <avr/eeprom.h>
+#include <sys/device.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+
+#include "system.h"
+#include "uart0driver.h"
+#include "log.h"
+#include "Store.h"
+
+#define LOG_MODULE  LOG_MAIN_MODULE
+#define SETTINGS_ADDR (void*)0x100
+
+char* text = "blaat";
+struct eeprom_data eeprom;
+void store_tests(void)
+{
+        char* tst = "XXXXX";
+        //writeWord((void*)46, 0x6948);
+        //readWord((void*)46, (void*)tst);
+        eeprom_write(text, (void*)46, 5);
+        eeprom_read(tst, (void*)46, 5);
+        printf("\nRead %s\n", tst);
+}
+
+void readByte(void* adr)
+{
+        uint8_t ByteOfData;
+        ByteOfData = eeprom_read_byte((uint8_t*)adr);
+        printf("\n%c\n", ByteOfData);  
+        
+}
+
+/**
+*Reading word from eeprom.
+*/
+void readWord(void* adr, uint16_t* data)
+{
+        *data = eeprom_read_word(adr);
+}
+
+/**
+*Write word to eeprom.
+*/
+void writeWord(void* adr, uint16_t data)
+{
+        eeprom_write_word(adr, data);
+}
+
+void readBlock(void)
+{
+        uint8_t StringOfData[10];
+        eeprom_read_block((void*)&StringOfData, (const void*)46, 10);
+        printf("\n Words are %s\n", StringOfData);
+}
+
+void eeprom_read(void* data, void* addr, size_t num)
+{
+        int i = 0;
+        for (; i < num; i++, addr++)
+        {
+                ((char*)data)[i] = eeprom_read_byte(addr);
+        }
+}
+
+void eeprom_write(void* data, void* out, size_t num)
+{
+        int i = 0;
+        for (; i < num; i++, out++)
+        {
+                eeprom_write_byte(out, ((char*)data)[i]);
+        }
+}
+
+void store_settings(void)
+{
+        eeprom_write(&eeprom, SETTINGS_ADDR, sizeof(eeprom));
+}
+
+void restore_settings(void)
+{
+        eeprom_read(&eeprom, SETTINGS_ADDR, sizeof(eeprom));
+}
